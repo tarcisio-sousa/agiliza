ee2037bc8443b97f29108597e9ab76e94b253da0
Tarcísio Sousa <sousa.tarcisio.s@gmail.com>
2021-08-18 09:39:58 -0300

    Script para adicionar elemento item a tabela

--- core/base/serializers.py
+++ core/base/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
-from .models import ProjetoControleItem
+from .models import ProjetoControleItem, Alternativa, Responsavel
+
+
+class AlternativaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alternativa
+        fields = ['id', 'descricao']
+
+
+class ResponsavelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Responsavel
+        fields = ['id', 'nome']
 
 
 class ItemControleSerializer(serializers.ModelSerializer):
+    # alternativa = AlternativaSerializer()
+
+    class Meta:
+        model = ProjetoControleItem
+        fields = ['id', 'controle', 'item', 'alternativa', 'responsavel', 'observacoes', 'comentario', 'data_prevista']
+
+
+class ItemControleViewSerializer(serializers.ModelSerializer):
+    alternativa = AlternativaSerializer()
+    responsavel = ResponsavelSerializer()
+
     class Meta:
         model = ProjetoControleItem
         fields = ['id', 'controle', 'item', 'alternativa', 'responsavel', 'observacoes', 'comentario', 'data_prevista']
