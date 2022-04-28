161e7f3c18f3cd33049782b1cc40a37d66d04d46
Tarcísio Sousa <sousa.tarcisio.s@gmail.com>
2022-04-28 12:24:32 -0300

    Adicionando situação de convênios

--- core/base/static/js/convenios.js
+++ core/base/static/js/convenios.js
@@ -1,10 +1,52 @@
-// const url_base = ''
-const url_base = '/agiliza'
+const url_base = ''
+// const url_base = '/agiliza'
 const url_projeto = 'projeto'
+const url_convenio_aprovar_projeto = `convenio/aprovar/projeto`
+const url_convenio_licitar_projeto = `convenio/licitar/projeto`
+const url_convenio_analisar_licitacao = `convenio/analisar/licitacao`
+const url_convenio_aprovar_licitacao = `convenio/aprovar/licitacao`
+const url_convenio_recurso_conta = `convenio/recurso/em/conta`
+const url_convenio_concluir = `convenio/concluir`
+const url_concluir_prestacao_contas = `convenio/concluir/prestacao/contas`
 
 let seleciona_convenio = (elemento) => {
     let form_convenio_projeto = document.getElementById('formConvenioProjeto')
-    form_convenio_projeto.action=`${url_base}/${url_projeto}/${elemento.dataset.id}/`;
+    form_convenio_projeto.action =`${url_base}/${url_projeto}/${elemento.dataset.id}/`;
+}
+
+let aprovar_projeto_convenio = (elemento) => {
+    let form_convenio_aprovar_projeto = document.getElementById('formAprovarProjeto')
+    form_convenio_aprovar_projeto.action =`${url_base}/${url_convenio_aprovar_projeto}/${elemento.dataset.id}/`;
+}
+
+let licitar_projeto_convenio = (elemento) => {
+    let form_convenio_licitar_projeto = document.getElementById('formLicitarProjeto')
+    form_convenio_licitar_projeto.action =`${url_base}/${url_convenio_licitar_projeto}/${elemento.dataset.id}/`;
+}
+
+let analisar_licitacao_convenio = (elemento) => {
+    let form_convenio_analisar_licitacao = document.getElementById('formAnalisarLicitacao')
+    form_convenio_analisar_licitacao.action =`${url_base}/${url_convenio_analisar_licitacao}/${elemento.dataset.id}/`;
+}
+
+let aprovar_licitacao_convenio = (elemento) => {
+    let form_convenio_aprovar_licitacao = document.getElementById('formAprovarLicitacao')
+    form_convenio_aprovar_licitacao.action =`${url_base}/${url_convenio_aprovar_licitacao}/${elemento.dataset.id}/`;
+}
+
+let recurso_em_conta_convenio = (elemento) => {
+    let form_convenio_recurso_conta = document.getElementById('formRecursoConta')
+    form_convenio_recurso_conta.action =`${url_base}/${url_convenio_recurso_conta}/${elemento.dataset.id}/`;
+}
+
+let concluir_convenio = (elemento) => {
+    let form_concluir_convenio = document.getElementById('formConcluirConvenio')
+    form_concluir_convenio.action =`${url_base}/${url_convenio_concluir}/${elemento.dataset.id}/`;
+}
+
+let concluir_prestacao_contas = (elemento) => {
+    let form_concluir_prestacao_contas = document.getElementById('formConcluirPrestacaoContas')
+    form_concluir_prestacao_contas.action =`${url_base}/${url_concluir_prestacao_contas}/${elemento.dataset.id}/`;
 }
 
 let excluir_convenio = (item) => {
